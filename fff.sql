d3baa4a22322900d612afcb733e9756518fb1065
swyat <swyatyxa@i.ua>
2013-04-23 15:44:58 +0300

    paginator

--- fff.sql
+++ fff.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1
--- Время создания: Апр 16 2013 г., 22:54
+-- Время создания: Апр 23 2013 г., 14:23
 -- Версия сервера: 5.5.27
 -- Версия PHP: 5.4.7
 
@@ -28,27 +28,20 @@ SET time_zone = "+00:00";
 
 CREATE TABLE IF NOT EXISTS `korustyvach_info` (
   `id` int(5) NOT NULL AUTO_INCREMENT,
-  `login` text NOT NULL,
-  `password` text NOT NULL,
-  `code` text NOT NULL,
+  `login` varchar(50) NOT NULL,
+  `password` varchar(20) NOT NULL,
+  `code` varchar(20) NOT NULL,
   `permissions` varchar(10) NOT NULL,
   PRIMARY KEY (`id`),
   KEY `permissions` (`permissions`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=49 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=54 ;
 
 --
 -- Дамп данных таблицы `korustyvach_info`
 --
 
 INSERT INTO `korustyvach_info` (`id`, `login`, `password`, `code`, `permissions`) VALUES
-(41, 'ddd', 'ddd', '588527aa90', ''),
-(42, 'swyat', '15011992', '5582a0cd78', 'admin'),
-(43, 'swiat', 'swiat', 'f8aa141299', ''),
-(44, 'ssss', 'sssss', 'ec1c3a9085', ''),
-(45, 'swt', 'swt', 'f7a1a7e561', ''),
-(46, 'Walera', 'qweqwe', '7c0b030bf5', ''),
-(47, 'sssss', 'sssss', 'a138882fd5', ''),
-(48, 'xxx', 'xxx', '82cab6d299', '');
+(53, 'swyat', '15011992', '6ed84cdbd8', 'admin');
 
 -- --------------------------------------------------------
 
@@ -68,35 +61,7 @@ CREATE TABLE IF NOT EXISTS `messages_info` (
   PRIMARY KEY (`id`),
   KEY `topic` (`topic`),
   KEY `id` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=112 ;
-
---
--- Дамп данных таблицы `messages_info`
---
-
-INSERT INTO `messages_info` (`id`, `name`, `email`, `topic`, `s_text`, `l_text`, `c_time`, `e_time`) VALUES
-(111, 'Swyatffddsfdf', 'swyatyxa@i.ua', 'tematika', 'jugjsdfhdghnfxzzhbdnfgnwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww...', 'jugjsdfhdghnfxzzhbdnfgnwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwqqqdsfvsdf', '2013-04-16 08:50:54', '0000-00-00 00:00:00');
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `swyat`
---
-
-CREATE TABLE IF NOT EXISTS `swyat` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `name` varchar(50) NOT NULL,
-  `email` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COMMENT='Цю таблицю не брати до уваги' AUTO_INCREMENT=3 ;
-
---
--- Дамп данных таблицы `swyat`
---
-
-INSERT INTO `swyat` (`id`, `name`, `email`) VALUES
-(1, 'Swyat', 'swyatyxa@i.ua'),
-(2, '', '');
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=31 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
